d18a2a58779352ffeceecfb176266c2b91ef4bf6
Code-W-A <radu.p1995@yahoo.com>
2026-01-02 19:36:36 +0200

    asdsa

--- src/components/Service/ServiceLayout.tsx
+++ src/components/Service/ServiceLayout.tsx
@@ -9,12 +9,6 @@ export default function ServiceLayout({ service }: { service: Service }) {
     <>
       <section className="bg-gray-50 pt-[90px] pb-20">
         <div className="container">
-          {/* 
-            Desktop behavior:
-            - Left column floats next to the content.
-            - After the left column ends, the content naturally expands to full width.
-            This is pure layout/CSS and does not affect SEO content.
-          */}
           <div className="-mx-5 lg:clearfix">
             <div className="w-full px-5 lg:float-left lg:w-4/12">
               <div className="space-y-10">
